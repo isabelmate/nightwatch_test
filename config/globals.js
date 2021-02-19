eb58525590aafff02a94bb6b1599fdfb126ec889
isabelmate <36552251+isabelmate@users.noreply.github.com>
2021-02-19 11:15:39 +0100

    Create globals.js

---
+++ config/globals.js
@@ -0,0 +1,13 @@
+var globalis = {
+    "aprilDateRates" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?idtokenprovider=100376648&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&deviceType=DESKTOP_TABLET&checkin=06%2F04%2F2020&nights=4&hsri=02040&currency=EUR",
+    "mayDateRates" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?idtokenprovider=100376648&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&deviceType=DESKTOP_TABLET&checkin=18%2F05%2F2020&nights=3&hsri=02040&currency=EUR",
+    "aprilDateNorates" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?device=DESKTOP_TABLET&idtokenprovider=100376647&nights=4&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&hsri=02040&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&checkin=06%2F04%2F2020&currency=EUR",
+    "mayDateNorates" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?idtokenprovider=100376647&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&deviceType=DESKTOP_TABLET&checkin=18%2F05%2F2020&nights=3&hsri=02040&currency=EUR",
+    "noDispoRates" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?idtokenprovider=100376648&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&deviceType=DESKTOP_TABLET&checkin=11%2F02%2F2020&nights=2&hsri=02040&currency=EUR#noAvailability",
+    "noDispoNorates" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?idtokenprovider=100376647&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&deviceType=DESKTOP_TABLET&checkin=11%2F02%2F2020&nights=2&hsri=02040&currency=EUR#noAvailability",
+    "mobile" : "https://reservation.mirai.com/XMAS/reservation/default_room_selection.htm?idtokenprovider=100376648&clientCodeStrictSearch=true&parties=W3siYWR1bHRzIjoyLCJjaGlsZHJlbiI6W119XQ%3D%3D&lang=en&home=https%3A%2F%2Fwww.mirai.com%2F&step=1&release=latest&deviceType=MOBILE&checkin=06%2F04%2F2020&nights=4&hsri=02040&currency=EUR"
+}
+
+module.exports = {
+    globalis: globalis
+  }
